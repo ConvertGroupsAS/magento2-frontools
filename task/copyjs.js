b33de422ab6296bd3a322173e27f6e2e6f64dc7c
Anastasia Gradoboeva <nastya@convert.no>
2016-08-15 20:21:27 +0300

    Added "gulp bower", "gulp copyjs". Updated "gulp deploy". Required "npm update"

--- task/copyjs.js
+++ task/copyjs.js
@@ -30,24 +30,7 @@ module.exports = function() { // eslint-disable-line func-names
       theme.locale.forEach(locale => {
         const src       = config.projectPath + theme.src,
               dest      = config.projectPath + theme.dest + '/' + locale,
-              srcPaths  = plugins.globby.sync(src + '/**/web/**', { nodir: true, ignore: '/**/node_modules/**' });
-
-        if (theme.parent) {
-          const parentTheme     = config.themes[theme.parent],
-                parentSrc       = config.projectPath + parentTheme.src,
-                parentSrcPaths  = plugins.globby.sync(parentSrc + '/**/web/**', { nodir: true, ignore: '/**/node_modules/**' });
-
-          parentSrcPaths.forEach(srcPath => {
-            const destPath = srcPath.replace('/web', '').replace(parentSrc, dest);
-            try {
-              plugins.fs.ensureFileSync(destPath);
-              plugins.fs.unlinkSync(destPath);
-            }
-            finally {
-              prod ? plugins.fs.copySync(srcPath, destPath) : plugins.fs.symlinkSync(srcPath, destPath);
-            }
-          });
-        }
+              srcPaths  = plugins.globby.sync(src + '/bower_components/**/**/**.js', { nodir: true, ignore: '/**/node_modules/**' });
 
         srcPaths.forEach(srcPath => {
           const destPath = srcPath.replace('/web', '').replace(src, dest);
@@ -62,6 +45,4 @@ module.exports = function() { // eslint-disable-line func-names
       });
     }
   });
-
-  this.opts.plugins.runSequence('copyjs');
 };
